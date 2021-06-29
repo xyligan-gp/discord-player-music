662c3723d55631b5531072c81e76582645912548
xyligan <48912672+xyligan-gp@users.noreply.github.com>
2021-06-29 10:58:35 +0300

    Remove unnecessary code

--- example/events/ready.js
+++ example/events/ready.js
@@ -1,5 +1,4 @@
 const MusicBot = require('../classes/Client.js');
-const fetch = require('node-fetch');
 
 module.exports = {
     name: 'ready',
@@ -8,21 +7,6 @@ module.exports = {
      * @param {MusicBot} client Discord Client
      */
     run: async(client) => {
-        setTimeout(version, 10);
-
-        function version() {
-            fetch('https://registry.npmjs.com/discord-player-music').then(res => res.json()).then(result => {
-                client.user.setActivity({ name: `Version: ${result['dist-tags'].latest}`, type: 'STREAMING', url: 'https://twitch.tv/twitch' })
-            })
-
-            setTimeout(users, 10000);
-        }
-
-        function users() {
-            client.user.setActivity({ name: `Users: ${client.users.cache.size}`, type: 'STREAMING', url: 'https://twitch.tv/twitch' });
-            setTimeout(version, 10000);
-        }
-
         return console.log(`${client.user.tag} ready!`);
     }
 }
