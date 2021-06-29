3f46ec7fcef39ed97e18fde35cfb1f594900014e
xyligan <48912672+xyligan-gp@users.noreply.github.com>
2021-06-29 10:56:03 +0300

    Fix errors

--- example/events/player/songAdded.js
+++ example/events/player/songAdded.js
@@ -8,6 +8,6 @@ module.exports = {
      * @param {import('discord-player-music').Song} song Event Data
      */
     run: async(client, song) => {
-        return data.textChannel.send(`New Song Added To Queue!\n\nSong Title: **${song.title}**\nSong URL: **${song.url}**\nSong Duration: **${song.duration.hours}:${song.duration.minutes}:${song.duration.seconds}**`);
+        return song.textChannel.send(`New Song Added To Queue!\n\nSong Title: **${song.title}**\nSong URL: **${song.url}**\nSong Duration: **${song.duration.hours}:${song.duration.minutes}:${song.duration.seconds}**`);
     }
 }
