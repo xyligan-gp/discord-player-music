ab544b91e03e6f6815daeddb2d968d7bf4be0308
xyligan <48912672+xyligan-gp@users.noreply.github.com>
2021-07-31 12:56:12 +0300

    [DEV] Update PlayerErrors

--- src/PlayerErrors.js
+++ src/PlayerErrors.js
@@ -5,6 +5,7 @@ module.exports = {
 
     voiceManager: {
         userVoiceNotFound: `User '{userID}' not found in voice channel!`,
+        connectionNotFound: `Connection on server '{guildID}' not found!`,
 
         clientInVoice: `Client '{clientTag}' is already in voice channel '{voiceName}'!`,
         clientNotInVoice: `Client '{clientTag}' not found in the voice channel '{voiceName}'!`
