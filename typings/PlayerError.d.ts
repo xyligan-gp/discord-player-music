efbd9d713e228d85815d2cf14c0cefe768b81d88
xyligan <48912672+xyligan-gp@users.noreply.github.com>
2022-07-25 18:04:42 +0300

    Delete typings directory

--- typings/PlayerError.d.ts
+++
@@ -1,7 +0,0 @@
-declare class DiscordPlayerMusicError extends Error {
-    constructor(message: string | Error);
-
-    public name: 'DiscordPlayerMusic Error';
-}
-
-export = DiscordPlayerMusicError;
